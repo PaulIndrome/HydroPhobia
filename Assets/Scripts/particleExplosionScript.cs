95916f069f502d6317b071ae8819af4340974caf
Paul Indrome <fpschocke@gmail.com>
2017-12-03 01:05:27 +0100

    Project Setup

---
+++ Assets/Scripts/particleExplosionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleExplosionScript : MonoBehaviour {
+
+	public GameObject playerObject;
+	public ParticleSystem particleBlip;
+	public ParticleSystem particleSplurt;
+	private float particleSystemDuration;
+
+	public void Start(){
+		Destroy(gameObject, 1.2f);
+	}
+
+	public void setTriggerObject(GameObject player){
+		playerObject = player;
+		transform.LookAt(playerObject.transform);
+		transform.Rotate(Vector3.forward, 180f);
+	}
+
+
+}
---
+++ Assets/Scripts/particleScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleScript : MonoBehaviour {
+
+	public PlayerScript player;
+	public GameObject particleExplosionPrefab;
+	private ParticleSystem ps;
+	private List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+	private int particleTriggers = 0;
+	private Vector3 particleExile = Vector3.negativeInfinity;
+
+
+	public void onEnabled(){
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	public void OnParticleTrigger(){
+		if(ps == null)
+			ps = GetComponent<ParticleSystem>();
+
+		//int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+		ParticlePhysicsExtensions.GetTriggerParticles(ps, ParticleSystemTriggerEventType.Enter, enter);
+
+		//for(int i = 0; i < numEnter; i++){
+		//	enter[i].
+		//	Debug.Log("ParticleTrigger " + particleTriggers++ + " at " + enter[i].position);
+		//	GameObject pEx = Instantiate(particleExplosion, enter[i].position, Quaternion.identity);
+		//	pEx.GetComponent<particleExplosionScript>().setTriggerObject(player.gameObject);
+		//}
+
+		foreach(ParticleSystem.Particle p in enter){
+				Debug.Log("ParticleTrigger " + particleTriggers++ + " at " + p.position + " with StartLifeTime of " + p.startLifetime + " ID " + p.GetHashCode());
+				GameObject pEx = Instantiate(particleExplosionPrefab, p.position, Quaternion.identity);
+				pEx.GetComponent<particleExplosionScript>().setTriggerObject(player.gameObject);
+		}
+
+		
+
+		
+		
+	}
+
+
+
+}
